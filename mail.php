ae81552e6b80dbc5df820c3f0f4d4ba754badf4a
jsrasheduzzaman <jsrasheduzzaman02@gmail.com>
2024-05-07 22:21:33 +0600

    Try to add mailgun php sdk

--- mail.php
+++ mail.php
@@ -1,54 +1,72 @@
 <?php
 
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-        $name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $subject = trim($_POST["subject"]);
-        $number = trim($_POST["number"]);
-        $message = trim($_POST["message"]);
+    // // Only process POST reqeusts.
+    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //     // Get the form fields and remove whitespace.
+    //     $name = strip_tags(trim($_POST["name"]));
+    //     $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    //     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    //     $subject = trim($_POST["subject"]);
+    //     $number = trim($_POST["number"]);
+    //     $message = trim($_POST["message"]);
 
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR empty($number) OR empty($subject) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Please complete the form and try again.";
-            exit;
-        }
+    //     // Check that data was sent to the mailer.
+    //     if ( empty($name) OR empty($message) OR empty($number) OR empty($subject) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    //         // Set a 400 (bad request) response code and exit.
+    //         http_response_code(400);
+    //         echo "Please complete the form and try again.";
+    //         exit;
+    //     }
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "webfydev@gmail.com";
+    //     // Set the recipient email address.
+    //     // FIXME: Update this to your desired email address.
+    //     $recipient = "webfydev@gmail.com";
 
-        // Set the email subject.
-        $subject = "New contact from $subject";
+    //     // Set the email subject.
+    //     $subject = "New contact from $subject";
 
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Subject: $subject\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
+    //     // Build the email content.
+    //     $email_content = "Name: $name\n";
+    //     $email_content .= "Subject: $subject\n";
+    //     $email_content .= "Email: $email\n\n";
+    //     $email_content .= "Message:\n$message\n";
 
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
+    //     // Build the email headers.
+    //     $email_headers = "From: $name <$email>";
 
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
-        }
+    //     // Send the email.
+    //     if (mail($recipient, $subject, $email_content, $email_headers)) {
+    //         // Set a 200 (okay) response code.
+    //         http_response_code(200);
+    //         echo "Thank You! Your message has been sent.";
+    //     } else {
+    //         // Set a 500 (internal server error) response code.
+    //         http_response_code(500);
+    //         echo "Oops! Something went wrong and we couldn't send your message.";
+    //     }
 
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
-    }
+    // } else {
+    //     // Not a POST request, set a 403 (forbidden) response code.
+    //     http_response_code(403);
+    //     echo "There was a problem with your submission, please try again.";
+    // }
+
+
+    require 'vendor/autoload.php'; // Include Mailgun SDK
+
+    use Mailgun\Mailgun;
+
+    // Initialize Mailgun object with your API key and domain
+    $mg = Mailgun::create('75cd784d-bbe8f4c1');
+    $domain = "webfydev.com";
+
+    // Send email
+    $mg->messages()->send($domain, [
+        'from'    => 'wefydev@gmail.com',
+        'to'      => 'jsrasheduzzaman02@gmail.com',
+        'subject' => 'Test Email via Mailgun PHP API',
+        'text'    => 'This is a test email sent via Mailgun PHP API!'
+    ]);
+
 
 ?>
